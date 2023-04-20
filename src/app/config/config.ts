2df0c635f4ab688f60b554f255ecb0621c0b031e
Bjorn 'Bjeaurn <bjorn@somnu.com>
2023-04-20 11:38:57 +0200

    feat: Adds an injectionToken and sets that up so its filled before app boot.

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,9 @@
+import { InjectionToken } from '@angular/core';
+
 export type Config = {
   apiUrl: string;
 };
+
+export const APP_CONFIG: InjectionToken<Config> = new InjectionToken<Config>(
+  'Application Config'
+);
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { APP_CONFIG } from './app/config/config';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+fetch('./assets/config/config.json')
+  .then((resp) => resp.json())
+  .then((config) => {
+    platformBrowserDynamic([{ provide: APP_CONFIG, useValue: config }])
+      .bootstrapModule(AppModule)
+      .catch((err) => console.error(err));
+  });
