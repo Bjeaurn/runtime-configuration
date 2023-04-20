29a991fabebd663a7710a34672366e7f21cc3666
Bjorn 'Bjeaurn <bjorn@somnu.com>
2023-04-20 11:41:55 +0200

    feat: Creates an example of how to use the config service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ConfigService } from './config/config.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'runtime-cfg';
+
+  constructor(private cfg: ConfigService) {
+    console.log(this.cfg);
+  }
 }
